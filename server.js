667bc5b91f768158e9103f0f714fb581426294d9
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-17 21:13:35 +0500

    Test Server

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-/** @format */
+// /** @format */
 
-import app from './app.js';
+// import app from './app.js';
 
-import connectDatabase from './config/database.js';
+// import connectDatabase from './config/database.js';
 
-connectDatabase();
+// connectDatabase();
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+// 	console.log(`Server is running on port ${PORT}`);
+// });
+
+
+import express from 'express';
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5001;
+
+//Server Status
+app.get('/', (req, res) => {
+	res.status(200).send('Server is Up on Vercel!');
+});
+
+app.listen(port, () => {
+	console.log(`Node/Express Server is Up......\nPort: localhost:${port}`);
 });
