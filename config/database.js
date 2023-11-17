d7861c3d61e115d2c21d6ac489caa7261fb3f0b5
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-18 00:57:50 +0500

    DB connection problem fix

--- config/database.js
+++ config/database.js
@@ -3,15 +3,17 @@
 import { connect } from 'mongoose';
 
 const connectDatabase = () => {
-	connect(process.env.DB_URI2, {})
-		.then((data) => {
-			console.log(
-				`Connected to database at ${data.connection.host}:${data.connection.port}/${data.connection.name}`,
-			);
-		})
-		.catch((error) => {
-			console.error('Database connection error:', error);
-		});
+	return new Promise((resolve, reject) => {
+		connect(process.env.DB_URI2, {})
+			.then((data) => {
+				console.log(`Connected to database at ${data.connection.host}:${data.connection.port}/${data.connection.name}`);
+				resolve(data);
+			})
+			.catch((error) => {
+				console.error('Database connection error:', error);
+				reject('Database connection error');
+			});
+	});
 };
 
 export default connectDatabase;
--- index.js
+++ index.js
@@ -4,9 +4,18 @@ import app from './app.js';
 
 import connectDatabase from './config/database.js';
 
-connectDatabase();
+connectDatabase()
+	.then(() => {
+		const PORT = process.env.PORT;
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error('Failed to connect to the database:', error);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+		// Send a 500 Internal Server Error response
+		app.use((err, req, res, next) => {
+			res.status(500).json({ error: 'Database connection error' });
+		});
+	});
