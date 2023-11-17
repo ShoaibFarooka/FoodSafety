d26e5eaefec490fa6e3383e2fdadd537b3c7725b
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-17 21:44:51 +0500

    Testing real server with name index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import express from 'express';
-const app = express();
-app.use(express.json());
-const port = process.env.PORT || 5001;
+/** @format */
 
-//Server Status
-app.get('/', (req, res) => {
-	res.status(200).send('Server is Up on Vercel!');
-});
+import app from './app.js';
 
-app.listen(port, () => {
-	console.log(`Node/Express Server is Up......\nPort: localhost:${port}`);
+import connectDatabase from './config/database.js';
+
+connectDatabase();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
