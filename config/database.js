aa101cd5e05887cf8235a89b54059d2ea158989e
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-18 01:08:15 +0500

    DB connection problem fix

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDatabase = () => {
 	// connect(process.env.DB_URI2, {})
-	connect(process.env.DB_URI2, {})
+	connect('mongodb+srv://shoaibfarooka:Welcome5home.@cluster0.hrpczac.mongodb.net/?retryWrites=true&w=majority', {})
 		.then((data) => {
 			console.log(
 				`Connected to database at ${data.connection.host}:${data.connection.port}/${data.connection.name}`,
