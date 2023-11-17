eb5e473a3305d53d4f351f1059b45df326cbcf28
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-18 02:38:20 +0500

    webhooks data format fix

--- routes/StripeIntegration.js
+++ routes/StripeIntegration.js
@@ -2,6 +2,6 @@ import express from 'express';
 import { IntegratePayment, StripeOrderStatus, StripeHooks } from '../Controller/StripeIntegration.js';
 const router = express.Router();
 router.route('/create-checkout-session').post(IntegratePayment);
-router.route('/webhooks').post(StripeHooks);
+router.post('/webhooks', express.raw({ type: 'application/json' }), StripeHooks);
 router.route('/status/:userID').get(StripeOrderStatus);
 export default router;
