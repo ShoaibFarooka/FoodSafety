ee07bb3137b6eb7c1d0f5336dbc5914bd7386fd4
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-18 01:36:45 +0500

    .env fix

--- config/database.js
+++ config/database.js
@@ -5,8 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDatabase = () => {
-	// connect(process.env.DB_URI2, {})
-	connect('mongodb+srv://shoaibfarooka:Welcome5home.@cluster0.hrpczac.mongodb.net/?retryWrites=true&w=majority', {})
+	connect(process.env.DB_URI2, {})
 		.then((data) => {
 			console.log(
 				`Connected to database at ${data.connection.host}:${data.connection.port}/${data.connection.name}`,
